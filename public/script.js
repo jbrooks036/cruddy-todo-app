32492008930ef373f1f008b9cb680c7332c960ea
jbrooks036 <joannbrooks036@gmail.com>
2016-05-26 17:11:12 -0500

    WIP: almost working login and authentication

--- public/script.js
+++ public/script.js
@@ -1,5 +1,6 @@
 $(() => {
 	const API_URL = "https://cruddy-todo-app-eaa32.firebaseio.com/task"
+  let token = null
 
   // 'data-id' is js attribute, not css 'id'
   const addItemToTable = (item, id) => {
@@ -14,32 +15,23 @@ $(() => {
     $('tbody').append(row)
   }
 
-  // GET
-  $.get(`${API_URL}.json`)
-    .done((data) => {
-      if (data) {
-        // for (id in data) {
-        //    add ItemToTable(data[id])
-        // }
-        Object.keys(data).forEach((id) => {
-          addItemToTable(data[id], id)
-        })
-      }
-      //TODO:  handle completed tasks
-    })
-
-    // CREATE:  form submit event to POST data to firebase
-  $('form').submit(() =>
+  // CREATE:  form submit event to POST data to firebase
+  $('.new form').submit(() => {
     // $.ajax({
-    //   url: API_URL, 
+    //   url: API_URL.json, 
     //   method: 'POST',
-    //   data: JSON.stringify({ task: 'I was posted!'}) // needed for Firebase
+    //   data: JSON.stringify({ task: 'I was posted!'}) // 'stringify' needed for Firebase
     // })
-
-    // or could use this instead: 
-    $.post(`${API_URL}.JSON`, 
-      JSON.stringify({ task: 'I was posted!' }))
+    // or use this instead: 
+    $.post(`${API_URL}.json?auth=${token}`, 
+      JSON.stringify({ task: 'I was posted!' })
+    )
     // TODO:  Grab the form text
+    $('.add-item').on("submit", (e) => {
+      console.log("add-item button clicked")
+      alert( "Handler for .add-item() called." );
+      e.preventDefault();
+    })
     // TODO: Make this not refresh the page
   })
 
@@ -47,14 +39,88 @@ $(() => {
     const row = $(e.target).closest('tr')
     const id = row.data('id')
     debugger
-       // which button got clicked?
+    
+    // which button got clicked?
     $.ajax({
-      url:`${API_URL}/${id}.json`,
+      url:`${API_URL}/${id}.json?auth=${token}`,
       method: 'DELETE'
     }).done(() => {
       row.remove()
     })
   })
+
+  firebase.initializeApp({
+    apiKey: "AIzaSyBu7HFGByove02FkMMcYtVd8SpuojM8KGU",
+    authDomain: "cruddy-todo-app-eaa32.firebaseapp.com",
+    databaseURL: "https://cruddy-todo-app-eaa32.firebaseio.com",
+    storageBucket: "cruddy-todo-app-eaa32.appspot.com",
+  })
+
+  // both return promise like objects
+  const login = (email, password) => (
+    firebase.auth()
+    .signInWithEmailAndPassword(email, password)
+  )
+
+  const register = (user, password) => (
+    firebase.auth().createUserWithEmailAndPassword(user, password)
+  )
+
+  $('.login form').submit((e) => {
+    const form = $(e.target)
+    const email = form.find('input[type="text"]').val()
+    const password = form.find('input[type="password"]').val()
+
+    login(email, password)
+    .then(console.log)
+    .then(console.err)
+
+    e.preventDefault()
+  })
+
+  $('input[value="Register"]').click((e) => {
+    const form = $(e.target).closest('form')
+    const email = form.find('input[type="text"]').val()
+    const password = form.find('input[type="password"]').val()
+
+    register(email, password)
+      .then(() => login(email, password))
+      .then(console.log)
+      .catch(console.err)
+
+    e.preventDefault()
+  })
+
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      //logged in
+      $('.app').show()
+      token = user._lat
+      getTasks()
+    }else {
+      // not logged in
+      $('.login').show()
+    }
+  })
+
+  
+// READ: GET data from firebase and display in table
+  const getTasks = () => {
+    $.get(`${API_URL}.json?auth=${token}`)
+      .done((data) => {
+        console.log("data = ", data)
+        if (data) {
+          // for (id in data) {
+          //    add ItemToTable(data[id])
+          // }
+
+          Object.keys(data).forEach((id) => {
+            addItemToTable(data[id], id)
+          })
+        }
+        //TODO:  handle completed tasks
+      })
+  }
 })
 
 // CREATE(implement 2nd): form submit event to POST data to firebase
